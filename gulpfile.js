738a9ecd12babbe2c944d7c1ada853f60fc4560a
ALEX LIM <alim@ccs.neu.edu>
2018-03-04 14:33:24 -0500

    set up gulp build

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+
+gulp.task('hello', function() {
+  console.log('Hello Alex');
+});
+
+gulp.task('less', function () {
+  return gulp.src('app/less/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('watch', ['browserSync', 'less'], function (){
+  gulp.watch('app/less/**/*.less', ['less']);
+  // Other watchers
+  gulp.watch('app/*.html', browserSync.reload);
+  // gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
